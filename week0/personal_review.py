4134a5a5feb54db97d7663f06d11a2ab0b8ca44f
boccassinisergio-afk <boccassini.sergio@gmail.com>
2026-04-02 20:22:23 +0200

    primo commit

---
+++ week0/personal_review.py
@@ -0,0 +1,11 @@
+def main():
+    name = input("what's your name? ").strip().title()
+    surname = input("what's your surname? ").strip().title()
+    x = round(float(input(f"hello {name} {surname}, what's X value? ")))
+    print(f"dear {name}, {x} squared is {square(x)}")
+    print(f"and {x} cubed is {cube(x)}")
+def square(x):
+    return x * x 
+def cube(x):
+    return x * x * x
+main()
